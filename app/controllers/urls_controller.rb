16c04f503f4bfe159918780e6460d827077e5406
Estelle <estelleccl@gmail.com>
2017-01-23 06:59:57 +0800

    added ajax form submission

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,29 +1,34 @@
 class UrlsController < ApplicationController
+
+	respond_to :js, :html
+
 	def new
 		@url = Url.new
 	end
 
 	def create
-		@long_url = Url.new(url_params)
+		@url = Url.new(url_params)
 
-		if @long_url.save
+		if @url.save
 			flash[:notice] = 'Successfully created shorter url.'
-			redirect_to root_path
+			respond_to do |format|  
+        format.js { render 'urls/show_update', long_url: @url.long_url, short_url: @url.short_url}
+    	end  
 		else
 			flash[:alert] = "Ops! Something wrong with the url. Remember it need http/http"
-			redirect_to root_path
+			redirect_to "/url.#{@url.id}"		
 		end
 	end
 
 	def show		
-		@url = Url.find_by(short_url: params[:short_url])
-		if @url.nil?
-			flash[:alert] = "Ops! Something wrong with the url. Remember it need http/http"
-			redirect_to root_path
-		else
-			@short_url.counter
-			redirect_to @url.short_url
-		end
+		# @url = Url.find_by(short_url: params[:short_url])
+		# if @url.nil?
+		# 	flash[:alert] = "Ops! Something wrong with the url. Remember it need http/http"
+		# 	redirect_to root_path
+		# else
+		# 	@short_url.counter
+		# 	redirect_to @url.short_url
+		# end
 	end
 
 	private
