fd6e43df44d1c8866ee537224ab0a1d6ae0dde2c
Estelle <estelleccl@gmail.com>
2017-01-23 14:10:03 +0800

    ammended redis.rb v1.1

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,5 @@
-uri = URI.parse(ENV["REDISCLOUD_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if Rails.env == 'production'
+  $redis = Redis.new(url: ENV["REDISCLOUD_URL"])
+else
+  $redis = Redis.new
+end
