2fd8eba10347cfc9b40c532722663d356481e4a5
Estelle <estelleccl@gmail.com>
2017-01-23 14:13:20 +0800

    verified able to work on heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,3 @@
-if Rails.env == 'production'
-  $redis = Redis.new(url: ENV["REDISCLOUD_URL"])
-else
-  $redis = Redis.new
-end
+ENV["REDISTOGO_URL"] ||= "redis://rediscloud:z34jmwe6geRgT6op@redis-15584.c11.us-east-1-3.ec2.cloud.redislabs.com:15584"
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
